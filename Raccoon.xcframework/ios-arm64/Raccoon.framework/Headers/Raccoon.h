a826eac219cb526a949fe2f04a49dc881a205f8d
scorchy38 <vkumarsaraswat@gmail.com>
2024-05-03 13:56:26 +0000

    Releasing version 0.0.1

---
+++ Raccoon.xcframework/ios-arm64/Raccoon.framework/Headers/Raccoon.h
@@ -0,0 +1,18 @@
+//
+//  Raccoon.h
+//  Raccoon
+//
+//  Created by SHUBH  SARASWAT on 16/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Raccoon.
+FOUNDATION_EXPORT double RaccoonVersionNumber;
+
+//! Project version string for Raccoon.
+FOUNDATION_EXPORT const unsigned char RaccoonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Raccoon/PublicHeader.h>
+
+
